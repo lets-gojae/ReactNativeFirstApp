988e21f1b516edc304627663b1c008d177f33205
ko jae won <sis9410@gmail.com>
2021-02-08 16:24:17 +0900

    done

--- Counter/src/Screens/Counter/index.tsx
+++ Counter/src/Screens/Counter/index.tsx
@@ -2,11 +2,62 @@ import React, {useState} from 'react';
 import Styled from 'styled-components/native';
 import Button from '~/Components/Button';
 
+interface Props {
+  title?: string;
+  initValue: number;
+}
+
+const Counter = ({title, initValue}: Props) => {
+  const [count, setCount] = useState<number>(0);
+
+  return (
+    <Container>
+      {title && (
+        <TitleContainer>
+          <TitleLabel>{title}</TitleLabel>
+        </TitleContainer>
+      )}
+      <CounterContainer>
+        <CountLabel>{initValue + count}</CountLabel>
+      </CounterContainer>
+      <ButtonContainer>
+        <Button iconName="plus" onPress={() => setCount(count + 1)} />
+        <Button iconName="minus" onPress={() => setCount(count - 1)} />
+      </ButtonContainer>
+    </Container>
+  );
+};
+
+export default Counter;
+
 const Container = Styled.SafeAreaView`
   flex: 1;
 `;
 
 const TitleContainer = Styled.View`
   flex: 1;
-  justify-conten
+  justify-content: center;
+  align-items: center;
+`;
+
+const TitleLabel = Styled.Text`
+  font-size: 24px;
+`;
+
+const CounterContainer = Styled.View`
+  flex: 2;
+  justify-content: center;
+  align-items: center;
+`;
+
+const CountLabel = Styled.Text`
+  font-size: 24px;
+  font-weight: bold;
+`;
+
+const ButtonContainer = Styled.View`
+  flex: 1;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-around;
 `;
