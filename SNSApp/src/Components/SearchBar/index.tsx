56975460179394f3bbc0472622b21a1c05921c0e
ko jae won <sis9410@gmail.com>
2021-05-09 22:55:54 +0900

    wip

---
+++ SNSApp/src/Components/SearchBar/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Styled from 'styled-components/native';
+
+import IconButton from '~/Components/IconButton';
+import Input from '~/Components/Input';
+
+const SearchBar = () => {
+  return (
+    <Container>
+      <Input style={{flex: 1, marginLeft: 8, heigth: 32}} placeholder="검색" />
+      <IconButton iconName="camera" />
+    </Container>
+  );
+};
+
+export default SearchBar;
+
+const Container = Styled.SafeAreaView`
+  flex:1;
+  flex-direction: row;
+  align-items: center;
+  background-color: #FEFFFF;
+`;
