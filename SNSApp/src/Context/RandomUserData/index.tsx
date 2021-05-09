d33b8bbf2f2e14711ea7760b3e5b90f48ca5a766
ko jae won <sis9410@gmail.com>
2021-05-09 18:02:01 +0900

    wip

--- SNSApp/src/Context/RandomUserData/index.tsx
+++ SNSApp/src/Context/RandomUserData/index.tsx
@@ -2,7 +2,7 @@ import React, {createContext, useState, useEffect} from 'react';
 import {Image} from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
 
-import Loading from '~/components/Loading';
+import Loading from '~/Components/Loading';
 
 interface Props {
   cache?: boolean;
