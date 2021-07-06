ea314c72aa11f9c8597bb64133b61015db7c94c2
ko jae won <sis9410@gmail.com>
2021-07-07 00:16:01 +0900

    WIP

---
+++ Outstagram/src/Context/RandomUserData/index.tsx
@@ -0,0 +1,11 @@
+import React, {createContext, useState, useEffect} from 'react';
+import {Image} from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
+
+interface Props {}
+
+const RandomUserDataProvider = ({}: Props) => {
+  return;
+};
+
+export {RandomUserDataProvider};
